725601d9feadeb6667de0acca29919b03343d42b
Hector Abimael Centeno Barba <hector@dommo.mx>
2022-06-08 11:36:42 -0500

    Remove files now working

---
+++ init.lua
@@ -0,0 +1,29 @@
+require "options"
+require "user.keymaps"
+require "user.plugins"
+require "user.cmp"
+require "user.lsp"
+require "user.treesitter"
+require "user.autopairs"
+require "user.bufferline"
+
+-- vim.cmd "colorscheme sonokai"
+vim.cmd "colorscheme nightfox"
+
+
+-- Setup Nightfox - Try to use Nightfox
+--
+-- local nightfox = require('nightfox')
+-- nightfox.setup({
+--     fox = 'nightfox',
+--     alt_nc = true,
+--    visual = true,
+--    search = true,
+--    styles =  {
+--        comments = "italic",
+--        keywords = "bold",
+--        functions = "italic,bold"
+--    }
+-- })
+-- nightfox.load()
+
