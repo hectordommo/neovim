6fa753bd5c165f4083411809715c16ae4e3bc8f7
Hector Abimael Centeno Barba <hector@dommo.mx>
2023-11-16 11:20:50 -0600

    make blankline work again

--- lua/user/blankline.lua
+++ lua/user/blankline.lua
@@ -1,10 +1,11 @@
-local status_ok, blankline = pcall(require, "indent_blankline")
+local status_ok, blankline = pcall(require, "ibl")
 if not status_ok then
   return
 end
 
 vim.opt.list = true
 blankline.setup {
-  show_current_context = true,
-  show_current_context_start = true,
+  indent = { highlight = highlight, char = "" },
+--  show_current_context = true,
+--  show_current_context_start = true,
 }
