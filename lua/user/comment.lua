b9c3f139d80f64bf8efdf20ac7398131d2693f8c
Hector Abimael Centeno Barba <hector@dommo.mx>
2023-12-11 14:35:02 -0600

    use default comment toogle actions

--- lua/user/comment.lua
+++ lua/user/comment.lua
@@ -4,19 +4,25 @@ if not status_ok then
     return
 end
 
-comment.setup {
-    pre_hook = function(ctx)
-        local U = require "Comment.utils"
-        local location = nil
-        if ctx.ctype == U.ctype.block then
-            location = require("ts_context_commentstring.utils").get_cursor_location()
-        elseif ctx.cmotion == U.cmotion or ctx.cmotion == U.cmotion.V then
-            location = require("ts_context_commentstring.utils").get_visual_start_location()
-        end
+comment.setup ()
 
-    return require("ts_context_commentstring.internal").calculate_commentstring {
-            key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-            location = location,
-        }
-    end,
-}
+                --[[ 
+                --line = '<leader>cc',
+                --block = '<leader>bc', ]]
+
+--[[ comment.setup { ]]
+--[[     pre_hook = function(ctx) ]]
+--[[         local U = require "Comment.utils" ]]
+--[[         local location = nil ]]
+--[[         if ctx.ctype == U.ctype.block then ]]
+--[[             location = require("ts_context_commentstring.utils").get_cursor_location() ]]
+--[[         elseif ctx.cmotion == U.cmotion or ctx.cmotion == U.cmotion.V then ]]
+--[[             location = require("ts_context_commentstring.utils").get_visual_start_location() ]]
+--[[         end ]]
+--[[]]
+--[[     return require("ts_context_commentstring.internal").calculate_commentstring { ]]
+--[[             key = ctx.ctype == U.ctype.line and "__default" or "__multiline", ]]
+--[[             location = location, ]]
+--[[         } ]]
+--[[     end, ]]
+--[[ } ]]
