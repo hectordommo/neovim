61d8afb094a9e71b35a547bf0951fa100ce43e5c
Hector Centeno <hector@dommo.mx>
2024-05-17 16:54:40 -0600

    Change null-ls for none-ls via mason

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,21 +1,4 @@
-local null_ls_status_ok, null_ls = pcall(require, "null-ls")
-if not null_ls_status_ok then
-  return
-end
-
---     formatting.prettier.with { extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
-
-null_ls.setup {
-  debug = true,
-  sources = {
-    null_ls.builtins.diagnostics.eslint,
-
-    -- formatting.phpcsfixer,
-    -- formatting.black.with { extra_args = { "--fast" } },
-    -- formatting.yapf,
-    -- formatting.stylua,
-    -- diagnostics.flake8,
-  },
-}
-
-
+require("mason").setup()
+require("mason-null-ls").setup({
+    handlers = {},
+})
