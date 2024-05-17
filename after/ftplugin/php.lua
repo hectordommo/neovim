01cddb0974de2865ff5365cdaa75ba6c02238515
Hector Centeno <hector@dommo.mx>
2024-05-17 16:54:56 -0600

    several updates for upgrading to Nvim 9

---
+++ after/ftplugin/php.lua
@@ -0,0 +1,3 @@
+vim.opt_local.shiftwidth = 4                          -- the number of spaces inserted for each indentation
+vim.opt_local.softtabstop = 4
+vim.opt_local.tabstop = 4                             -- with of the spaces for a tab
