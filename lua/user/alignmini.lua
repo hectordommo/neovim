b955459fc8ac76f4d1b9fe5ffea8c3ff2a7f5fc8
Hector Centeno <hcenteno@MacBookPro.local>
2026-04-05 15:12:52 -0600

    remove pluings file from old package manager

--- lua/user/alignmini.lua
+++
@@ -1,7 +0,0 @@
--- Setup MINI automatic align tool
-local status_ok, ref = pcall(require, "mini.align")
-if not status_ok then
-  return
-end
-
-ref.setup()
--- lua/user/markid.lua
+++
@@ -1,6 +0,0 @@
-local status_ok, markid = pcall(require, "markid")
-if not status_ok then
-  return
-end
-
--- markid.init()
