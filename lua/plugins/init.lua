905858e163d98382e5b02a778d326fdb68c86cf4
Hector Centeno <hcenteno@MacBookPro.local>
2026-04-05 15:13:05 -0600

    better configuration for better ts erros

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -85,7 +85,16 @@ return {
       require("neoclip").setup()
     end,
   },
-  { "OlegGulevskyy/better-ts-errors.nvim", lazy = true },
+  {
+    "OlegGulevskyy/better-ts-errors.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    config = {
+      keymaps = {
+        toggle = '<leader>dd', -- default '<leader>dd'
+        go_to_definition = '<leader>dx' -- default '<leader>dx'
+      }
+    }
+  },
   { 
     "alexghergh/nvim-tmux-navigation", lazy = true,
     config = function()
