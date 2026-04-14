d90956a96669d1176d58af748d14a38c40d17188
Hector Centeno <hector@dommo.mx>
2026-04-14 17:14:59 -0600

    disable mark id

--- lua/plugins/markid.lua
+++ lua/plugins/markid.lua
@@ -1,4 +1,25 @@
 return {
   "David-Kunz/markid",
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
   enabled = false,
+  config = function ()
+    local m = require'markid'
+    require'nvim-treesitter.configs'.setup({
+      markid = { 
+        enable = true,
+      }
+    })
+    m.queries = {
+      default = '(identifier) @markid',
+      javascript = [[
+      (identifier) @markid
+      (property_identifier) @markid
+      (shorthand_property_identifier_pattern) @markid
+      ]],
+      php = [[
+      (variable_name) @markid
+      ]]
+    }
+    m.queries.typescript = m.queries.javascript
+  end
 }
